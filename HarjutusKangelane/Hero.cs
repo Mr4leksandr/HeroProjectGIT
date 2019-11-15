9e24e50f0fb008cc5ae9e40af8583b98779efea3
opilane <opilane@HP-CZC6297B90>
2019-11-15 09:22:56 +0200

    HeroProject

---
+++ HarjutusKangelane/Hero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjutusKangelane
+{
+    //1. Klassis Kangelane privaatsed isendiväljad järgmise info jaoks: nimi (String), asukoht (String). Klassis on vähemalt üks konstruktor.
+    class Hero
+    {
+        private string _name;
+        private string _place;
+
+        public Hero(string name, string place)
+        {
+            _name = name;
+            _place = place;
+        }
+
+        // Kõigi isendiväljade jaoks on vastavad get-meetodid ja set-meetodid.
+        public string HeroName
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string HeroPlace 
+        {
+            get { return _place; }
+            set { _place = value;  }
+        }
+
+        // Meetod päästa (int-tüüpi), mille argumendiks on ohus olevate inimeste arv (int). Meetod tagastab päästetud inimeste arvu, mis on ohus olevate inimeste arvust 95 protsenti (ümardatuna täisarvuks).
+        public virtual int Save(int indanger)
+        {
+            return (int)(indanger * 0.95);
+        }
+
+        // On ka meetod toString, mis võimaldab kangelast mõistlikult ekraanil kujutada.
+        public virtual string ToString()
+        {
+            return $"{_name} / {_place} \nSaved: {Save(indanger)}\n";
+        }
+    }
+}
---
+++ HarjutusKangelane/SuperHero.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjutusKangelane
+{
+    //2. Klass SuperKangelane on klassi Kangelane alamklass. Klassis SuperKangelane on veel isendiväli osavuse kohta (double), mis kõigub vahemikus [1-5).
+    //Ülemklassis olemasolevaid isendivälju siin uuesti mitte kirjeldada.
+    class SuperHero : Hero
+    {
+        // Isendiväli (double)
+        private double _dexterity;
+
+        // Klassis on vähemalt üks konstruktor, mis muuhulgas määrab juhuslikult superkangelase osavuse.
+        public SuperHero(string name, string place, int dexterity) : base(name, place)
+        {
+            _dexterity = dexterity;
+        }
+
+        // Klassis SuperKangelane on veel isendiväli osavuse kohta (double), mis kõigub vahemikus [1-5).
+        public double DexterityValue()
+        {
+            Random r = new Random();
+            return _dexterity = r.Next(1, 5);
+        }
+
+        // Meetod päästa kaetakse üle nii, et meetod tagastab päästetud inimeste arvu, mis on ohus olevate inimeste arvust (95 + osavus) protsenti (ümardatuna täisarvuks).
+        public override int Save(int saved)
+        {
+            saved = 95 + (int)_dexterity;
+        }
+
+        // Meetodi toString ülekatmisel on rakendatud ülemklassi meetodit toString lisades osavuse väljastamise.
+        public override string ToString()
+        {
+            return base.ToString()+$"\nPeaple saved: ";
+        }
+    }
+}
